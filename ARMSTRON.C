8a71844caf38c193b1489ae376b996453c9c1aef
Nihar Koli <42617538+nihar-koli@users.noreply.github.com>
2019-03-23 21:16:31 +0530

    added first program of c

---
+++ ARMSTRON.C
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+void main()
+{
+  int num,rem,sum=0,a;
+  clrscr();
+   printf("\n\n Enter any num : ");
+    scanf("%d",&num);
+    a=num;
+
+
+   while(num>0)
+   {
+     rem=num%10;
+     sum=sum+pow(rem,3);
+     num=num/10;
+   }
+   if(sum==a)
+   printf("\n\n it is an armstrong number");
+   else
+   printf("\n\n it is not an armstrong number");
+
+  getch();
+}
+
