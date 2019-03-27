b5308c745ab3ee38e180f529c94abd2e15820687
Nihar Koli <42617538+nihar-koli@users.noreply.github.com>
2019-03-27 22:59:50 +0530

    Added fibonacci series program

---
+++ FIBONACC.C
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int a=0,b=1,c,n,i=3;
+  clrscr();
+
+   printf("\n\n Enter number of digits of fibonacci series you want : ");
+     scanf("%d",&n);
+   if(n>=1)
+   printf("%d ",a);
+   if(n>=2)
+   printf("%d",b);
+
+   while(i<=n)
+   {
+     c=a+b;
+     printf(" %d ",c);
+     a=b;
+     b=c;
+     i++;
+   }
+
+  getch();
+}
+
