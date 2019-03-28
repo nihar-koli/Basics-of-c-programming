b01139cd857a065a9257e207567dff68a4d3511c
Nihar Koli <42617538+nihar-koli@users.noreply.github.com>
2019-03-28 22:04:39 +0530

    added program to convert decimal no to binary no

---
+++ BINARYCO.C
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+void main()
+{
+  int num,rem,i=1,rev=0;
+  clrscr();
+   printf("\n\n Enter any num : ");
+    scanf("%d",&num);
+
+    while(num != 0)
+    {
+      rem=num%2;
+      rev=(rem*i)+rev;
+      num=num/2;
+      i *= 10;
+    }
+       printf("\n\n binary equivalent is %d ",rev);
+
+  getch();
+}
+
