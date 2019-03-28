456e97be93d9b40c285bfb1f8dad2d8661604c55
Nihar Koli <42617538+nihar-koli@users.noreply.github.com>
2019-03-28 22:19:06 +0530

    Update ARMSTRON.C

--- ARMSTRON.C
+++ ARMSTRON.C
@@ -20,9 +20,9 @@ void main()
      num=num/10;
    }
    if(sum==a)
-   printf("\n\n It is an armstrong number");
+   printf("\n\n %d is an armstrong number",a);
    else
-   printf("\n\n It is not an armstrong number");
+   printf("\n\n %d is not an armstrong number",a);
 
   getch();
 }
