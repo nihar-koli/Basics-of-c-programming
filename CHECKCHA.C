3ff263b6a0138c04028cb7ddd7a53cfaf5ae6560
Nihar Koli <42617538+nihar-koli@users.noreply.github.com>
2019-03-30 21:56:55 +0530

    added character checking program

---
+++ CHECKCHA.C
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+  char ch;
+  clrscr();
+    printf("\n\n Enter any character : ");
+      scanf("%c", &ch);
+
+   if(ch>='A' && ch<='Z')
+   {
+      printf("\n\n Entered character %c is uppercase",ch);
+   }
+   else if(ch>='a' && ch<='z')
+   {
+      printf("\n\n Entered character %c is lowercase",ch);
+   }
+   else if(ch>='0' && ch<='9')
+   {
+      printf("\n\n Entered character %c is number",ch);
+   }
+   else
+   {
+      printf("\n\n Invalid character");
+   }
+   getch();
+}
