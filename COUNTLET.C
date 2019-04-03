2398a824c881de4f5e766c3617a86d7fa52fb0da
Nihar Koli <42617538+nihar-koli@users.noreply.github.com>
2019-04-03 22:06:40 +0530

    added program to count letters in string

---
+++ COUNTLET.C
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+   char str[50];
+   int i,j,len,count;
+      clrscr();
+      printf("\n\n enter any string : ");
+	 gets(str);
+      len=strlen(str);
+	for(i=0;i<len;i++)
+	{
+	  count=1;
+	   for(j=i+1;j<len;j++)
+	   {
+	     if(str[i] == str[j])
+	     {
+	       count++;
+	       str[j]=' ';
+	     }
+
+	   }
+	   if(str[i] != ' ')
+	      printf("\n\n %c = %d",str[i],count);
+
+
+	}
+
+
+
+   getch();
+}
