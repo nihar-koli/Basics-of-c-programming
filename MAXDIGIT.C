06f4928a14f5ac0f7aab5db247d91f26d866c253
Nihar Koli <42617538+nihar-koli@users.noreply.github.com>
2019-04-06 22:36:38 +0530

    added program to find maximum digit out of number

---
+++ MAXDIGIT.C
@@ -0,0 +1,31 @@
+//binary search
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int num,rem,min,max;
+       clrscr();
+       printf("\n\n Enter any number : ");
+	 scanf("%d",&num);
+       min=9;
+       max=0;
+	 while(num != 0)
+	 {
+	   rem=num%10;
+	     if(rem<min)
+	     {
+		min=rem;
+	     }
+	     if(rem>max)
+	     {
+		max=rem;
+	     }
+	   num=num/10;
+	 }
+       printf("\n\n max number is : %d ",max);
+       printf("\n\n min number is : %d ",min);
+
+
+
+   getch();
+}
